8b0025850c92edc193bc8735380991ea8df670d9
Jop Maarse <jop_maarse@hotmail.com>
2021-12-03 23:54:38 +0100

    refactor

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -1,87 +1,90 @@
 ﻿using InputLogic;
 
 IEnumerable<string> input = await InputHelper.GetInputAsync(day: 3);
-int numberOfColumns = input.First().Length;
+int numberOfBits = input.First().Length;
 
 // Assignment 1
-int numberOfRows = input.Count();
-uint gammaRate = 0;
-for (int i = 0; i < numberOfColumns; i++)
+int numberOfReadings = input.Count();
+ushort gammaRate = 0;
+for (int i = 0; i < numberOfBits; i++)
 {
     int sum = input
         .Select(line => int.Parse(line.Substring(i, 1)))
         .Sum();
 
-    if (sum > (numberOfRows - sum))
-        gammaRate |= (uint)(1 << (11 - i));
+    if (sum > (numberOfReadings - sum))
+        gammaRate |= (ushort)(1 << (11 - i));
 }
 
-uint mask = 0b0000_0000_0000_0000_0000_1111_1111_1111;
-uint epsilonRate = ~gammaRate & mask;
+ushort mask = 0b0000_1111_1111_1111;
+int epsilonRate = ~gammaRate & mask;
 Console.WriteLine("The answer to assignment 1 is: " + epsilonRate * gammaRate);
 
 // Assignment 2
-int[] report = input.Select(s => Convert.ToInt32(s, 2)).ToArray();
-int oxygen = report.GetRating(new OxygenRating(), numberOfColumns);
-int co2 = report.GetRating(new CO2Rating(), numberOfColumns);
-
+int[] report = input.Select(s => Convert.ToInt32(s, fromBase: 2)).ToArray();
+int oxygen = new OxygenRating(report, numberOfBits).Value;
+int co2 = new CO2Rating(report, numberOfBits).Value;
 Console.WriteLine("The answer to assignment 2 is: " + oxygen * co2);
 
 static class Logic
 {
-    public static int GetRating(this int[] report, IRating measurement, int columns)
+    public static int FindMostCommonBit(this int[] values, int index)
     {
-        for (int i = 0; i < columns && report.Length > 1; i++)
-        {
-            int column = i;
-            Bit criteria = measurement.Criteria(report, column);
-            report = report.ApplyCriteriaToColumn(criteria, column).ToArray();
-        }
-
-        return report.Single();
+        int oneCount = values.Select(value => value.GetBitAtIndex(index)).Sum();
+        return Convert.ToInt32(oneCount >= values.Length - oneCount);
+    }
+    public static int GetBitAtIndex(this int number, int index)
+    {
+        number &= 1 << (11 - index);
+        return Math.Min(1, number);
     }
 
-    public static Bit FindMostCommonBit(this int[] values, int column)
-    {
-        int oneCount = values.Select(value => ColumnSelector(value, column)).Sum();
-        if (oneCount >= values.Length - oneCount)
-            return Bit.One;
-        return Bit.Zero;
-    }
+    public static IEnumerable<int> FilterOnIndex(this IEnumerable<int> numbers, int index, int value) =>
+        numbers.Where(number => number.GetBitAtIndex(index) == value);
 
-    public static IEnumerable<int> ApplyCriteriaToColumn(this IEnumerable<int> input, Bit criteria, int column) =>
-        input.Where(value => ColumnSelector(value, column) == (int)criteria);
-
-    public static int ColumnSelector(int value, int column)
-    {
-        value &= 1 << (11 - column);
-        return Math.Min(1, value);
-    }
-
-    public static Bit Flip(this Bit bit) => bit ^ (Bit)1;
-}
-
-enum Bit
-{
-    Zero = 0,
-    One = 1
+    public static int Flip(this int bit) => bit ^ 1;
 }
 
 interface IRating
 {
-    Bit Criteria(int[] values, int column);
+    int Value { get; }
 }
 
-class OxygenRating : IRating
+abstract class RatingBase : IRating
 {
-    public Bit Criteria(int[] values, int column) => values.FindMostCommonBit(column);
-}
+    protected abstract int GetCriteria(int[] values, int column);
 
-class CO2Rating : IRating
-{
-    public Bit Criteria(int[] values, int column)
-    {
-        Bit mostCommonBit = values.FindMostCommonBit(column);
-        return mostCommonBit.Flip();
+    public int Value { get; }
+
+    protected RatingBase(int[] report, int numberOfBits)
+    {        
+        for (int i = 0; i < numberOfBits && report.Length > 1; i++)
+        {
+            int index = i;
+            int criteria = GetCriteria(report, index);
+            report = report
+                .FilterOnIndex(index, criteria)
+                .ToArray();
+        }
+
+        Value = report.Single();
     }
+}
+
+class OxygenRating : RatingBase
+{
+    public OxygenRating(int[] report, int numberOfBits) : base(report, numberOfBits) { }
+
+    protected override int GetCriteria(int[] values, int index) =>
+        values.FindMostCommonBit(index);
+}
+
+class CO2Rating : RatingBase
+{
+    public CO2Rating(int[] report, int numberOfBits) : base(report, numberOfBits) { }
+
+    protected override int GetCriteria(int[] values, int index) =>
+        values
+            .FindMostCommonBit(index)
+            .Flip();
 }
