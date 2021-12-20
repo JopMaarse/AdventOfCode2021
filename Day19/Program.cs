d8d7a18dddf831469462c6343069200325ca0883
Jop Maarse <jop_maarse@hotmail.com>
2021-12-20 14:35:53 +0100

    Added day 20 solution (and day 19 WIP)

---
+++ Day19/Program.cs
@@ -0,0 +1,104 @@
+﻿using InputLogic;
+using System.Text.RegularExpressions;
+
+IEnumerable<string> input = await InputHelper.GetInputAsync(day: 19);
+
+List<Scanner> scanners = new();
+List<Beacon> beacons = new();
+Parse();
+InitScannerZero();
+
+void Parse()
+{
+    Regex scannerPattern = new(@"--- scanner ([0-9]+) ---");
+    int scannerId = -1;
+    foreach (string reading in input)
+    {
+        Match scannerMatch = scannerPattern.Match(reading);
+        if (scannerMatch.Success)
+        {
+            scannerId = int.Parse(scannerMatch.Groups[1].Value);
+            scanners.Add(new Scanner { Id = scannerId });
+            continue;
+        }
+
+        int[] coordinates = reading
+            .Split(',')
+            .Select(coordinate => int.Parse(coordinate))
+            .ToArray();
+
+        Vector location = new(coordinates[0], coordinates[1], coordinates[2]);
+        beacons.Add(new Beacon { ScannerId = scannerId, RelativeLocation = location });
+    }
+}
+
+void InitScannerZero()
+{
+    Scanner scannerZero = scanners.First(s => s.Id == 0);
+    scannerZero.Rotation = Rotation.Zero;
+    scannerZero.Direction = Direction.Forward;
+    foreach (Beacon beacon in beacons.Where(b => b.ScannerId == 0))
+    {
+        beacon.AbsoluteLocation = beacon.RelativeLocation;
+    }
+}
+
+class Scanner
+{
+    public int Id { get; set; }
+    public Vector? Location { get; set; }
+    public Direction? Direction { get; set; }
+    public Rotation? Rotation { get; set; }
+}
+
+class Beacon
+{
+    public int ScannerId { get; set; }
+    public Vector? RelativeLocation { get; set; }
+    public Vector? AbsoluteLocation { get; set; }
+}
+
+record struct Vector(int X, int Y, int Z)
+{
+    public Vector Translate(Direction direction, Rotation rotation) => Turn(Rotate(this, rotation), direction);
+
+    public static Vector operator -(Vector a, Vector b) => new(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+
+    private static Vector Turn(Vector vector, Direction direction) => direction switch
+    {
+        Direction.Forward => vector,
+        Direction.Backward => new(-vector.X, vector.Y, vector.Z),
+        Direction.Left => new(vector.Y, -vector.X, vector.Z),
+        Direction.Right => new(-vector.Y, vector.X, vector.Z),
+        Direction.Up => new(-vector.Z, vector.Y, vector.X),
+        Direction.Down => new(vector.Z, vector.Y, -vector.X),
+        _ => throw new Exception(),
+    };
+
+    private static Vector Rotate(Vector vector, Rotation rotation) => rotation switch
+    {
+        Rotation.Zero => vector,
+        Rotation.Quater => new(vector.X, vector.Z, -vector.Y),
+        Rotation.Half => new(vector.X, vector.Y, -vector.Z),
+        Rotation.ThreeQuater => new(vector.X, -vector.Z, vector.Y),
+        _ => throw new Exception()
+    };
+}
+
+enum Direction
+{
+    Forward,
+    Backward,
+    Left,
+    Right,
+    Up,
+    Down
+}
+
+enum Rotation
+{
+    Zero,
+    Quater,
+    Half,
+    ThreeQuater    
+}
---
+++ Day20/Program.cs
@@ -0,0 +1,57 @@
+﻿using InputLogic;
+
+IEnumerable<string> input = await InputHelper.GetInputAsync(day: 20);
+string imageEnhancementAlgorithm = input.First();
+string[] image = input.Skip(1).ToArray();
+const int passesPart1 = 2;
+const int passesPart2 = 50;
+bool flash = imageEnhancementAlgorithm[0] == '#' && imageEnhancementAlgorithm[^1] == '.';
+int part1 = LitPixelsAfterPasses(start: 0, end: passesPart1);
+Console.WriteLine("Part 1: " + part1);
+int part2 = LitPixelsAfterPasses(start: passesPart1, end: passesPart2);
+Console.WriteLine("Part 1: " + part2);
+
+int LitPixelsAfterPasses(int start, int end)
+{
+    for (int i = start; i < end; i++)
+        image = ProcessImage(imageEnhancementAlgorithm, image, padWithLit: flash && (i & 1) == 1);
+    
+    return image.Sum(line => line.Count(c => c == '#'));
+}
+
+static string[] ProcessImage(string imageEnhancementAlgorithm, string[] image, bool padWithLit)
+{
+    char pad = padWithLit ? '#' : '.';
+
+    image = image
+        .Prepend(new(pad, image[0].Length))
+        .Append(new(pad, image[0].Length))
+        .Select(line => $"{pad}{line}{pad}")
+        .ToArray();
+
+    return image
+        .Select((line, y) => line.Aggregate(string.Empty, (aggregate, _) => aggregate + imageEnhancementAlgorithm[ConvoluteKernel(image, (aggregate.Length, y), pad)]))
+        .ToArray();
+}
+
+static int ConvoluteKernel(string[] image, (int X, int Y) pixel, char pad)
+{
+    string read = 
+        (pixel.Y == 0 ? new(pad, 3) : ReadPixels(image[pixel.Y - 1], pixel.X, pad)) +
+        ReadPixels(image[pixel.Y], pixel.X, pad) +
+        (pixel.Y == image.Length - 1 ? new(pad, 3) : ReadPixels(image[pixel.Y + 1], pixel.X, pad));
+
+    return Convert.ToInt32(
+        read.Replace('.', '0')
+            .Replace('#', '1'),
+        fromBase: 2);
+}
+
+static string ReadPixels(string line, int x, char pad)
+{
+    if (x == 0)
+        return pad + line[x..(x + 2)];
+    if (x + 1 == line.Length)
+        return line[(x - 1)..(x + 1)] + pad;
+    return line[(x - 1)..(x + 2)];
+}
